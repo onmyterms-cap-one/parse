67b8cc787f8f591ea545dd9aa5e05bb461dc75f8
Ashok Parasa <aparasa@calpoly.edu>
2019-08-08 15:50:21 -0400

    On hold

---
+++ creditcard.py
@@ -0,0 +1,2 @@
+def credit_parse(text):
+    print(text)
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from tika import parser
+import creditcard
 
-def parsePDF(path="C://Users//HHS//Desktop//AlaskaAirlines.pdf"):
+def parsePDF(path):
     file_data = parser.from_file(path)
     text = file_data['content']
     return text
 
 def execute(category, text):
-    if(category == 'Airlines'):
-        airline_parse()
+    # if(category == 'Airlines'):
+    #     airline_parse()
     if(category == 'Credit Card'):
-        creditcard_parse()
+        creditcard.credit_parse(text)
 
 
 if __name__ == '__main__':
-    text = parsePDF()
-    execute("Airlines", text)
+    text = parsePDF("wellsfargo.pdf")
+    execute("Credit Card", text)
