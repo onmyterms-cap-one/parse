5f3579a71d052dec4916b8a16525dd3244c259be
Ashok Parasa <aparasa@calpoly.edu>
2019-08-09 00:56:37 -0400

    JSONLs

--- label_data.py
+++ label_data.py
@@ -8,7 +8,11 @@ import re
 def removeHTML(str):
     p = re.compile(r'<.*?>')
     return p.sub('', str)
-    
+
+def removeNewLine(str):
+    p = re.compile(r'\\n')
+    return p.sub('', str)
+
 f = []
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', '2015', '2016', '2017', '2018', '2019']
@@ -16,30 +20,40 @@ months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus
 for (dirpath, dirnames, filenames) in walk("service/"):
     f.extend(filenames)
     break
-workbook = xlsxwriter.Workbook('training.xlsx')
-worksheet = workbook.add_worksheet()
-row = 0
-col = 0
+# workbook = xlsxwriter.Workbook('training.xlsx')
+# worksheet = workbook.add_worksheet()
+# row = 0
+# col = 0
 for JSON in f:
     with open(("service/" + JSON), "r", encoding="utf8") as file:
-        data = json.load(file)
-        data = data['pointsData']
-        for key in data.keys():
-            obj = data[key]
-            if isinstance(obj, collections.Mapping) and "quoteText" in obj:
-                desc = obj['quoteText']
-                desc = removeHTML(desc)
-                hasDate = False
-                for month in months:
-                    if month.lower() in desc.lower():
-                        hasDate = True
-                rating = obj['tosdr']['point']
-                if not hasDate and desc != "" and desc != " " and desc != "Generated through the annotate view":
-                    print(desc)
-                    # print(obj['id'])
-                    worksheet.write(row, col, desc)
-                    worksheet.write(row, col+1, rating)
-                    # worksheet.write(row, col+2, obj['id'])
-                    row += 1
-workbook.close()
+        with open("onefile.jsonl", "a") as file2:
+            data = json.load(file)
+            data = data['pointsData']
+            count = 1
+            for key in data.keys():
+                obj = data[key]
+                if isinstance(obj, collections.Mapping) and "quoteText" in obj:
+                    desc = obj['quoteText']
+                    desc = removeHTML(desc)
+                    desc = removeNewLine(desc)
+                    hasDate = False
+                    for month in months:
+                        if month.lower() in desc.lower():
+                            hasDate = True
+                    rating = obj['tosdr']['point']
+                    if not hasDate and desc != "" and desc != " " and desc != "Generated through the annotate view":
+                        print(desc)
+                        # print(obj['id'])
+                        jsonl = {
+                            "text_snippet":
+                              {"content": desc}
+                          }
+                        json.dump(jsonl, file2)
+                        file2.write("\n")
+                        count += 1
+                        # worksheet.write(row, col, desc)
+                        # worksheet.write(row, col+1, rating)
+                        # worksheet.write(row, col+2, obj['id'])
+                        # row += 1
+# workbook.close()
 
